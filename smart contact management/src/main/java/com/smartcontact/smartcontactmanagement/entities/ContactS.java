86432f1d9e1f020a7805899bd1f2914fb4dd5439
Rushi Warade <rushiwarade023@gmail.com>
2024-06-14 22:46:55 +0530

    create all Entities that required for project . User,contacts,socialLinks

---
+++ smart contact management/src/main/java/com/smartcontact/smartcontactmanagement/entities/ContactS.java
@@ -0,0 +1,49 @@
+package com.smartcontact.smartcontactmanagement.entities;
+
+import java.util.*;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class ContactS {
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String picture;
+    @Column(length = 5000)
+    private String description;
+    private boolean favorite = false;
+
+    private String websiteLink;
+    private String linkdinLink;
+    // private List<String> socialLink = new arrayList<>();
+
+    @ManyToOne
+    private User user;
+
+    @OneToMany(mappedBy = "contacts", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<SocialLinks> links = new ArrayList<>();
+
+}
---
+++ smart contact management/src/main/java/com/smartcontact/smartcontactmanagement/entities/Providers.java
@@ -0,0 +1,6 @@
+package com.smartcontact.smartcontactmanagement.entities;
+
+public enum Providers {
+
+    SELF, GOOGLE, GITHUB
+}
